03eb8c5ad0af60cfc1d9e4bd5cdbdc109b4fb83c
Nikulin <nikulin.and90@gmail.com>
2022-12-28 00:47:32 +0200

    components/interface

---
+++ my-app/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+const Header = () => {
+  return <h2>Hello world</h2>
+}
+
+const Field = () => {
+  const holder = 'Enter here';
+  const styledField = {
+    width: '300px'
+  }
+  return <input placeholder={holder}
+   type='text'
+   style={styledField}/>
+}
+
+function Btn() {
+  const text = 'Log in';
+  const logged = true;
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Header/>
+      <Field/>
+      <Btn/>
+    </div>
+  );
+}
+
+export default App;
